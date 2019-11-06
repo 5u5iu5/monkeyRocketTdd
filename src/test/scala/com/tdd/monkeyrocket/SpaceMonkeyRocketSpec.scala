03d33f072a0202d6e7aca32844acaa2be23c6448
Alvaro Del Pozo Cuenca <alvaro.pozo.cuenca@gmail.com>
2019-11-06 18:23:58 +0100

    Intentar aligerar la carga de pensar en el workshop añadiendo test

--- src/test/scala/com/tdd/monkeyrocket/SpaceMonkeyRocketSpec.scala
+++ src/test/scala/com/tdd/monkeyrocket/SpaceMonkeyRocketSpec.scala
@@ -5,7 +5,32 @@ import org.scalatest.{Matchers, WordSpec}
 class SpaceMonkeyRocketSpec extends WordSpec with Matchers {
 
   "SpaceMonkeyRocket" should {
-    "to be launched with 2 monkeys and default control panel configured" in {
+    "to be launched return true with 2 monkeys and default control panel configured" in {
+      throw new RuntimeException("Implement code please, you can start to creating the class named as SpaceMonekyRocket")
+    }
+  }
+
+  "SpaceMonkeyRocket" should {
+    "return false because doesn't have setup" in {
+      throw new RuntimeException("Implement code please, you can start to creating the class named as SpaceMonekyRocket")
+    }
+  }
+
+  // Why not?, I want to test my private auxiliary methods
+  "CheckSetup" should {
+    "return false because doesn't have setup" in {
+      throw new RuntimeException("Implement code please, you can start to creating the class named as SpaceMonekyRocket")
+    }
+  }
+
+  "CheckSetup" should {
+    "return true because has defaultSetup" in {
+      throw new RuntimeException("Implement code please, you can start to creating the class named as SpaceMonekyRocket")
+    }
+  }
+
+  "AdjustSetupByMonkeyFault" should {
+    "return true if is false as argument" in {
       throw new RuntimeException("Implement code please, you can start to creating the class named as SpaceMonekyRocket")
     }
   }
